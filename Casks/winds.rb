e5cdc55bfebf887b5790b28c8cfd5f092da86eae
Thibault Milan <thibault@neopixl.com>
2018-06-20 10:23:16 +0200

    Add Winds.app v2.1.0

---
+++ Casks/winds.rb
@@ -0,0 +1,10 @@
+cask 'winds' do
+  version '2.1.0'
+  sha256 '8d25e427d258183be058aaada8baacec6e97ae73862eb485394596db9464aa6b'
+
+  url 'https://s3.amazonaws.com/winds-2.0-releases/releases/Winds-2.1.0.dmg'
+  name 'Winds'
+  homepage 'https://getstream.io/winds/'
+
+  app ''
+end
